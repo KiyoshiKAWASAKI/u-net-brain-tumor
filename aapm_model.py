2ffce663ca4198408b3ccaa842308fb32d8c3677
KiyoshiKAWASAKI <meitanteirie@yahoo.co.jp>
2018-02-16 14:54:14 -0500

    add aapm dataset

---
+++ aapm_model.py
@@ -0,0 +1,94 @@
+# Please be noticed that unet code is borrowed from: 
+# https://github.com/zsdonghao/u-net-brain-tumor
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow import *
+import tensorlayer as tl
+from tensorlayer.layers import *
+
+layers = tf.contrib.layers
+arg_scope = tf.contrib.framework.arg_scope
+FLAGS = tf.app.flags.FLAGS
+
+# Use unet as generator
+def u_net(x, is_train=False, reuse=False, n_out=1):
+	_, nx, ny, nz = x.get_shape().as_list()
+	with tf.variable_scope("u_net", reuse=reuse):
+	    tl.layers.set_name_reuse(reuse)
+	    inputs = InputLayer(x, name='inputs')
+	    conv1 = Conv2d(inputs, 64, (3, 3), act=tf.nn.relu, name='conv1_1')
+	    conv1 = Conv2d(conv1, 64, (3, 3), act=tf.nn.relu, name='conv1_2')
+	    pool1 = MaxPool2d(conv1, (2, 2), name='pool1')
+	    conv2 = Conv2d(pool1, 128, (3, 3), act=tf.nn.relu, name='conv2_1')
+	    conv2 = Conv2d(conv2, 128, (3, 3), act=tf.nn.relu, name='conv2_2')
+	    pool2 = MaxPool2d(conv2, (2, 2), name='pool2')
+	    conv3 = Conv2d(pool2, 256, (3, 3), act=tf.nn.relu, name='conv3_1')
+	    conv3 = Conv2d(conv3, 256, (3, 3), act=tf.nn.relu, name='conv3_2')
+	    pool3 = MaxPool2d(conv3, (2, 2), name='pool3')
+	    conv4 = Conv2d(pool3, 512, (3, 3), act=tf.nn.relu, name='conv4_1')
+	    conv4 = Conv2d(conv4, 512, (3, 3), act=tf.nn.relu, name='conv4_2')
+	    pool4 = MaxPool2d(conv4, (2, 2), name='pool4')
+	    conv5 = Conv2d(pool4, 1024, (3, 3), act=tf.nn.relu, name='conv5_1')
+	    conv5 = Conv2d(conv5, 1024, (3, 3), act=tf.nn.relu, name='conv5_2')
+
+	    up4 = DeConv2d(conv5, 512, (3, 3), (nx/8, ny/8), (2, 2), name='deconv4')
+	    up4 = ConcatLayer([up4, conv4], 3, name='concat4')
+	    conv4 = Conv2d(up4, 512, (3, 3), act=tf.nn.relu, name='uconv4_1')
+	    conv4 = Conv2d(conv4, 512, (3, 3), act=tf.nn.relu, name='uconv4_2')
+	    up3 = DeConv2d(conv4, 256, (3, 3), (nx/4, ny/4), (2, 2), name='deconv3')
+	    up3 = ConcatLayer([up3, conv3], 3, name='concat3')
+	    conv3 = Conv2d(up3, 256, (3, 3), act=tf.nn.relu, name='uconv3_1')
+	    conv3 = Conv2d(conv3, 256, (3, 3), act=tf.nn.relu, name='uconv3_2')
+	    up2 = DeConv2d(conv3, 128, (3, 3), (nx/2, ny/2), (2, 2), name='deconv2')
+	    up2 = ConcatLayer([up2, conv2], 3, name='concat2')
+	    conv2 = Conv2d(up2, 128, (3, 3), act=tf.nn.relu,  name='uconv2_1')
+	    conv2 = Conv2d(conv2, 128, (3, 3), act=tf.nn.relu, name='uconv2_2')
+	    up1 = DeConv2d(conv2, 64, (3, 3), (nx/1, ny/1), (2, 2), name='deconv1')
+	    up1 = ConcatLayer([up1, conv1] , 3, name='concat1')
+	    conv1 = Conv2d(up1, 64, (3, 3), act=tf.nn.relu, name='uconv1_1')
+	    conv1 = Conv2d(conv1, 64, (3, 3), act=tf.nn.relu, name='uconv1_2')
+	    conv1 = Conv2d(conv1, n_out, (1, 1), act=tf.nn.sigmoid, name='uconv1')
+	return conv1
+
+
+def discriminator(inputs, is_train=True, reuse=False):
+    n_filter = 32
+    _, nx, ny, nz = inputs.get_shape().as_list()
+    print("nx, ny, nz : ", nx, ny, nz)
+    # c_dim = FLAGS.x_dim + FLAGS.y_dim # two gray-scale image, 2
+    # batch_size = FLAGS.batch_size
+    # print("D batch size:{}".format(batch_size))
+    w_init = tf.random_normal_initializer(stddev=0.02)
+    # gamma_init = tf.random_normal_initializer(1., 0.02) # for BNLayer
+    with tf.variable_scope("discriminator", reuse=reuse):
+        tl.layers.set_name_reuse(reuse)
+
+        net_in = InputLayer(inputs, name='in') # (512,512,2)
+
+        # Encoder
+        net_conv0 = Conv2d(net_in, n_filter, (3, 3), (2, 2), act=tf.nn.elu,
+                        padding='SAME', W_init=w_init, name='conv0') # (nx/2,ny/2,n)
+
+        net_conv1 = Conv2d(net_conv0, 2*n_filter, (3, 3), (2, 2), act=tf.nn.elu,
+                           padding='SAME', W_init=w_init, name='conv1') # (nx/4,ny/4,2*n)
+
+        net_conv2 = Conv2d(net_conv1, 4*n_filter, (3, 3), (2, 2), act=tf.nn.elu,
+                           padding='SAME', W_init=w_init, name='conv2') # (nx/8,ny/8,4*n)
+
+        print("net_conv2 shape:", net_conv2.outputs.get_shape())
+
+        # net_flat2 = FlattenLayer(net_conv2, name='flatten2')
+        # net_dense = DenseLayer(net_flat2, )
+
+        # Decoder
+        net_deconv2 = DeConv2d(net_conv2, 2*n_filter, (3, 3), (nx/4, ny/4), (2, 2), name='deconv2')
+        net_deconv1 = DeConv2d(net_deconv2, n_filter, (3, 3), (nx/2, ny/2), (2, 2), name='deconv1')
+        net_deconv0 = DeConv2d(net_deconv1, nz, (3, 3), (nx, ny), (2, 2), name='deconv0')
+
+    return net_deconv0.outputs
---
+++ aapm_train.py
@@ -0,0 +1,326 @@
+#! /usr/bin/python
+# -*- coding: utf8 -*-
+
+import tensorflow as tf
+import tensorlayer as tl
+import numpy as np
+import os, time
+import aapm_model as model
+
+def distort_imgs(data):
+    """ data augumentation """
+    x1, x2, x3, x4, y = data
+    # x1, x2, x3, x4, y = tl.prepro.flip_axis_multi([x1, x2, x3, x4, y],  # previous without this, hard-dice=83.7
+    #                         axis=0, is_random=True) # up down
+    x1, x2, x3, x4, y = tl.prepro.flip_axis_multi([x1, x2, x3, x4, y],
+                            axis=1, is_random=True) # left right
+    x1, x2, x3, x4, y = tl.prepro.elastic_transform_multi([x1, x2, x3, x4, y],
+                            alpha=720, sigma=24, is_random=True)
+    x1, x2, x3, x4, y = tl.prepro.rotation_multi([x1, x2, x3, x4, y], rg=20,
+                            is_random=True, fill_mode='constant') # nearest, constant
+    x1, x2, x3, x4, y = tl.prepro.shift_multi([x1, x2, x3, x4, y], wrg=0.10,
+                            hrg=0.10, is_random=True, fill_mode='constant')
+    x1, x2, x3, x4, y = tl.prepro.shear_multi([x1, x2, x3, x4, y], 0.05,
+                            is_random=True, fill_mode='constant')
+    x1, x2, x3, x4, y = tl.prepro.zoom_multi([x1, x2, x3, x4, y],
+                            zoom_range=[0.9, 1.1], is_random=True,
+                            fill_mode='constant')
+    return x1, x2, x3, x4, y
+
+def vis_imgs(X, y, path):
+    """ show one slice """
+    if y.ndim == 2:
+        y = y[:,:,np.newaxis]
+    assert X.ndim == 3
+    tl.vis.save_images(np.asarray([X[:,:,0,np.newaxis],
+        X[:,:,1,np.newaxis], X[:,:,2,np.newaxis],
+        X[:,:,3,np.newaxis], y]), size=(1, 5),
+        image_path=path)
+
+def vis_imgs2(X, y_, y, path):
+    """ show one slice with target """
+    if y.ndim == 2:
+        y = y[:,:,np.newaxis]
+    if y_.ndim == 2:
+        y_ = y_[:,:,np.newaxis]
+    assert X.ndim == 3
+    tl.vis.save_images(np.asarray([X[:,:,0,np.newaxis],
+        X[:,:,1,np.newaxis], X[:,:,2,np.newaxis],
+        X[:,:,3,np.newaxis], y_, y]), size=(1, 6),
+        image_path=path)
+
+def main(task='all'):
+    ## Create folder to save trained model and result images
+    save_dir = "checkpoint"
+    tl.files.exists_or_mkdir(save_dir)
+    tl.files.exists_or_mkdir("samples/{}".format(task))
+
+    ###======================== LOAD DATA ===================================###
+    # The data was preprocessed and saved in npy files
+    train_img_path = "../../../data/AAPM/npy_files/train_images.npy"
+    train_mask_path = "../../../data/AAPM/npy_files/train_mask.npy"
+    valid_img_path = "../../../data/AAPM/npy_files/valid_images.npy"
+    valid_mask_path = "../../../data/AAPM/npy_files/valid_images.npy"
+    
+    # Load npy files
+    X_train = np.load(train_img_path)
+    y_train = np.load(train_mask_path)
+    X_test = np.load(valid_img_path)
+    y_test = np.load(valid_mask_path)
+
+
+    ###======================== HYPER-PARAMETERS ============================###
+    batch_size = 10
+    lr = 0.0001 
+    # lr_decay = 0.5
+    # decay_every = 100
+    beta1 = 0.9
+    n_epoch = 25
+    print_freq_step = 1
+    kt = tf.Variable(0., trainable=False)
+    gamma = 0.75
+    lamda = 0.001
+
+    ###======================== SHOW DATA ===================================###
+    # show one slice
+    X = np.asarray(X_train[80])
+    y = np.asarray(y_train[80])
+    # print(X.shape, X.min(), X.max()) # (240, 240, 4) -0.380588 2.62761
+    # print(y.shape, y.min(), y.max()) # (240, 240, 1) 0 1
+    nw, nh, nz = X.shape
+    vis_imgs(X, y, 'samples/{}/_train_im.png'.format(task))
+    # show data augumentation results
+    for i in range(10):
+        x_flair, x_t1, x_t1ce, x_t2, label = distort_imgs([X[:,:,0,np.newaxis], X[:,:,1,np.newaxis],
+                X[:,:,2,np.newaxis], X[:,:,3,np.newaxis], y])#[:,:,np.newaxis]])
+        # print(x_flair.shape, x_t1.shape, x_t1ce.shape, x_t2.shape, label.shape) # (240, 240, 1) (240, 240, 1) (240, 240, 1) (240, 240, 1) (240, 240, 1)
+        X_dis = np.concatenate((x_flair, x_t1, x_t1ce, x_t2), axis=2)
+        # print(X_dis.shape, X_dis.min(), X_dis.max()) # (240, 240, 4) -0.380588233471 2.62376139209
+        vis_imgs(X_dis, label, 'samples/{}/_train_im_aug{}.png'.format(task, i))
+
+    with tf.device('/gpu:2'):
+        sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
+        with tf.device('/gpu:2'): #<- remove it if you train on CPU or other GPU
+            ###======================== DEFIINE MODEL =======================###
+            #tf.reset_default_graph()
+            ## nz is 4 as we input all Flair, T1, T1c and T2.
+            t_image = tf.placeholder('float32', [batch_size, nw, nh, nz], name='input_image')
+            ## labels are either 0 or 1
+            t_seg = tf.placeholder('float32', [batch_size, nw, nh, 1], name='target_segment')
+            
+            ## Training (using generator)
+            net = model.u_net(t_image, is_train=True, reuse=False, n_out=1)
+            net_result = net.outputs #[batch, 240, 240, 1]
+            
+            # Concat the  generated results with ground truth label
+            #concated = tf.concat([t_seg, net_result], axis=3) # [10, 240, 240, 2]
+            concated = tf.concat([t_image, net_result], axis=3) # [10, 240, 240, 5]
+            #real_target = tf.concat([t_seg, t_seg], axis=3)
+            real_target = tf.concat([t_image, t_seg], axis=3) # [10, 240, 240, 5]
+            
+            # Send concated tensor into discriminator
+            d_out = model.discriminator(concated, is_train=True, reuse = False)
+            d_real = model.discriminator(real_target, is_train=True, reuse = True)
+            
+            ## test inference
+            net_test = model.u_net(t_image, is_train=False, reuse=True, n_out=1)
+
+            ###======================== DEFINE LOSS =========================###
+            ## Train losses
+            """
+            out_seg = net_result
+            train_iou = tl.cost.iou_coe(out_seg, t_seg, axis=[0,1,2,3])
+            train_dice_hard = tl.cost.dice_hard_coe(out_seg, t_seg, axis=[0,1,2,3])
+            train_loss = 1 - tl.cost.dice_coe(out_seg, t_seg, axis=[0,1,2,3])
+            """
+            out_seg = net_result
+            train_iou = tl.cost.iou_coe(out_seg, t_seg, axis=3)
+            train_dice_hard = tl.cost.dice_coe(out_seg, t_seg, axis=3)
+            train_loss = 1 - tl.cost.dice_coe(out_seg, t_seg, axis=3)
+
+            recons_loss = 1 - tl.cost.dice_coe(d_out, concated, axis=[0,1,2,3])#, 'jaccard', epsilon=1e-5)
+            fake_iou_loss = tl.cost.iou_coe(d_out, concated, axis=[0,1,2,3])
+            #fake_dice_hard = tl.cost.dice_hard_coe(d_out, concated, axis=[0,1,2,3])
+            fake_dice_hard = tl.cost.dice_coe(d_out, concated, axis=3)
+
+            real_dice_loss = 1 - tl.cost.dice_coe(d_real, real_target, axis=[0,1,2,3])#, 'jaccard', epsilon=1e-5)
+            real_iou_loss = tl.cost.iou_coe(d_real, real_target, axis=[0,1,2,3])
+            #real_dice_hard = tl.cost.dice_hard_coe(d_real, real_target, axis=[0,1,2,3])
+            real_dice_hard = tl.cost.dice_coe(d_real, real_target, axis=3)
+
+            # Summary of G and D losses
+            G_loss = recons_loss
+            D_loss = real_dice_loss - kt * recons_loss
+            M = real_dice_loss+tf.abs(gamma * real_dice_loss - recons_loss)
+            k_update = kt.assign(kt + lamda * (gamma * real_dice_loss - recons_loss))
+
+            ## test losses
+            """
+            test_out_seg = net_test.outputs
+            test_dice_loss = 1 - tl.cost.dice_coe(test_out_seg, t_seg, axis=[0,1,2,3])#, 'jaccard', epsilon=1e-5)
+            test_iou_loss = tl.cost.iou_coe(test_out_seg, t_seg, axis=[0,1,2,3])
+            test_dice_hard = tl.cost.dice_hard_coe(test_out_seg, t_seg, axis=[0,1,2,3])
+            """
+            test_out_seg = net_test.outputs
+            test_dice_loss = 1 - tl.cost.dice_coe(test_out_seg, t_seg, axis=3)
+            test_iou_loss = tl.cost.iou_coe(test_out_seg, t_seg, axis=3)
+            test_dice_hard = tl.cost.dice_coe(test_out_seg, t_seg, axis=3)
+
+        ###======================== DEFINE TRAIN OPTS =======================###
+        g_vars = tl.layers.get_variables_with_name('u_net', True, True)
+        d_vars = tl.layers.get_variables_with_name('discriminator', True, True)
+        
+        with tf.device('/gpu:0'):
+            with tf.variable_scope('learning_rate'):
+                lr_v = tf.Variable(lr, trainable=False)
+            train_op = tf.train.AdamOptimizer(lr_v, beta1=beta1).minimize(train_loss, var_list=g_vars)
+
+        with tf.device('/gpu:0'):
+            with tf.variable_scope('learning_rate'):
+                lr_v = tf.Variable(lr, trainable=False)
+            g_op = tf.train.AdamOptimizer(lr_v, beta1=beta1).minimize(G_loss, var_list=g_vars)
+
+        with tf.device('/gpu:0'):
+            with tf.variable_scope('learning_rate'):
+                lr_v = tf.Variable(lr, trainable=False)
+            d_op = tf.train.AdamOptimizer(lr_v, beta1=beta1).minimize(D_loss, var_list=d_vars)
+
+        ###======================== LOAD MODEL ==============================###
+        tl.layers.initialize_global_variables(sess)
+        ## load existing model if possible
+        tl.files.load_and_assign_npz(sess=sess, name=save_dir+'/u_net_{}.npz'.format(task), network=net)
+
+        ###======================== TRAINING ================================###
+    for epoch in range(0, n_epoch+1):
+        epoch_time = time.time()
+        n_batch = 0
+        total_dice_fake, total_iou_fake, total_dice_hard_fake = 0, 0, 0
+        total_dice_real, total_iou_real, total_dice_hard_real = 0, 0, 0
+        dice_train, iou_train = 0, 0
+        
+        for batch in tl.iterate.minibatches(inputs=X_train, targets=y_train,
+                                    batch_size=batch_size, shuffle=True):
+            images, labels = batch
+            step_time = time.time()
+            ## data augumentation for a batch of Flair, T1, T1c, T2 images
+            # and label maps synchronously.
+            data = tl.prepro.threading_data([_ for _ in zip(images[:,:,:,0, np.newaxis],
+                    images[:,:,:,1, np.newaxis], images[:,:,:,2, np.newaxis],
+                    images[:,:,:,3, np.newaxis], labels)],
+                    fn=distort_imgs) # (10, 5, 240, 240, 1)
+            b_images = data[:,0:4,:,:,:]  # (10, 4, 240, 240, 1)
+            b_labels = data[:,4,:,:,:]
+            b_images = b_images.transpose((0,2,3,1,4))
+            b_images.shape = (batch_size, nw, nh, nz)
+
+            ## update network
+            _, _train_dice, _train_iou = sess.run(
+                                    [train_op, train_dice_hard, train_iou],
+                                    {t_image: b_images, t_seg: b_labels})
+            dice_train += _train_dice
+            iou_train += _train_iou
+
+            # Run generater
+
+            _, _fakedice, _fakeiou, \
+            _fakediceh, out = sess.run([g_op, recons_loss, fake_iou_loss,
+                                    fake_dice_hard,net.outputs],
+                                    {t_image: b_images, t_seg: b_labels})
+            total_dice_fake += _fakedice
+            total_iou_fake += _fakeiou
+            total_dice_hard_fake += _fakediceh
+            
+            # Run discriminator
+            _, _realdice, \
+            _realiou, _realdiceh = sess.run([d_op, real_dice_loss,
+                                real_iou_loss, real_dice_hard],
+                                {t_image: b_images, t_seg: b_labels})
+            total_dice_real += _realdice
+            total_iou_real += _realiou
+            total_dice_hard_real += _realdiceh
+
+
+            # update k and losses
+            _, convergence_metric, kt_for_print = sess.run([k_update, M, kt], 
+                                                feed_dict={t_image: b_images, t_seg: b_labels})
+
+            _, loss_G = sess.run([g_op, G_loss],
+                                {t_image: b_images, t_seg: b_labels})
+
+            _, loss_D = sess.run([g_op, D_loss],
+                                {t_image: b_images, t_seg: b_labels})
+
+            n_batch += 1
+
+            if n_batch % print_freq_step == 0:
+
+                print("Epoch %d step %d. G loss: %f; D loss: %f; M is %f; kt is %f"
+                % (epoch, n_batch, loss_G, loss_D, convergence_metric, kt_for_print))
+                """
+                print("Fake dice-hard: %f; Fake IOU: %f"
+                % (total_dice_fake/n_batch, total_iou_fake/n_batch))
+                print("Real dice-hard: %f; Real IOU: %f"
+                % (total_dice_real/n_batch, total_iou_real/n_batch))
+                """
+                print("Fake dice-hard: %f; Fake IOU: %f"
+                % (_fakediceh, _fakeiou))
+                print("Real dice-hard: %f; Real IOU: %f"
+                % (_realdiceh, _realiou))
+
+            ## check model fail
+            if np.isnan(loss_G):
+                exit(" ** NaN loss found during training, stop training")
+            if np.isnan(loss_D):
+                exit(" ** NaN loss found during training, stop training")
+            if np.isnan(out).any():
+                exit(" ** NaN found in output images during training, stop training")
+
+        #print(" ** Epoch [%d/%d] train 1-dice: %f hard-dice: %f iou: %f took %fs (2d with distortion)" %
+                #(epoch, n_epoch, total_dice/n_batch, total_dice_hard/n_batch, total_iou/n_batch, time.time()-epoch_time))
+        
+
+        print(("Train dice: %f. Train IOU: %f") 
+            % (dice_train/n_batch, iou_train/n_batch))
+
+        ## save a predition of training set
+        for i in range(batch_size):
+            if np.max(b_images[i]) > 0:
+                vis_imgs2(b_images[i], b_labels[i], out[i], "samples/{}/train_{}.png".format(task, epoch))
+                break
+            elif i == batch_size-1:
+                vis_imgs2(b_images[i], b_labels[i], out[i], "samples/{}/train_{}.png".format(task, epoch))
+
+        ###======================== EVALUATION ==========================###
+        total_dice, total_iou, total_dice_hard, n_batch = 0, 0, 0, 0
+        for batch in tl.iterate.minibatches(inputs=X_test, targets=y_test,
+                                        batch_size=batch_size, shuffle=True):
+            b_images, b_labels = batch
+            _dice, _iou, _diceh, out = sess.run([test_dice_loss,
+                    test_iou_loss, test_dice_hard, net_test.outputs],
+                    {t_image: b_images, t_seg: b_labels})
+            total_dice += _dice; total_iou += _iou; total_dice_hard += _diceh
+            n_batch += 1
+
+        print("Test 1-dice: %f hard-dice: %f iou: %f (2d no distortion)" %
+                (total_dice/n_batch, total_dice_hard/n_batch, total_iou/n_batch))
+        print(" task: {}".format(task))
+        ## save a predition of test set
+        for i in range(batch_size):
+            if np.max(b_images[i]) > 0:
+                vis_imgs2(b_images[i], b_labels[i], out[i], "samples/{}/test_{}.png".format(task, epoch))
+                break
+            elif i == batch_size-1:
+                vis_imgs2(b_images[i], b_labels[i], out[i], "samples/{}/test_{}.png".format(task, epoch))
+
+        ###======================== SAVE MODEL ==========================###
+        tl.files.save_npz(net.all_params, name=save_dir+'/u_net_{}.npz'.format(task), sess=sess)
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--task', type=str, default='all', help='all, necrotic, edema, enhance')
+
+    args = parser.parse_args()
+
+    main(args.task)
